25d67dab475dd003cfa11afb68f51829e4bcef8b
ZISRF <zeleninivans@gmail.com>
2023-03-03 15:10:07 +0300

    delete: trash

--- 4sem/1lab/src/main/java/BanksRemake/Domain/Clocks/ClockSubscriber.java
+++
@@ -1,9 +0,0 @@
-package banksRemake.domain.clocks;
-
-/**
- * An object that responds to clock notifications.
- */
-public interface ClockSubscriber {
-    void reactOnDayChanged();
-    void reactOnMonthChanged();
-}
