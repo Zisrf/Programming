c578b0b49f5eabe5da530802eced30d599fa4a7c
ZISRF <zeleninivans@gmail.com>
2023-05-31 20:43:57 +0300

    fix

--- 4sem/3lab/friendlyPusses.pusses/src/main/java/friendlyPusses/pusses/config/KafkaConfig.java
+++ 4sem/3lab/friendlyPusses.pusses/src/main/java/friendlyPusses/pusses/config/KafkaConfig.java
@@ -1,6 +1,5 @@
-package friendlyPusses.webApi.config;
+package friendlyPusses.pusses.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
--- 4sem/3lab/friendlyPusses.users/src/main/java/friendlyPusses/users/config/KafkaConfig.java
+++ 4sem/3lab/friendlyPusses.users/src/main/java/friendlyPusses/users/config/KafkaConfig.java
@@ -10,23 +10,19 @@ import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 
 @Configuration
 public class KafkaConfig {
-    @Value("${friendlyPusses.reply-topics}")
-    private String REPLY_TOPICS;
-
     @Bean
     public ReplyingKafkaTemplate<String, Object, Object> replyingTemplate(
             ProducerFactory<String, Object> pf,
             ConcurrentMessageListenerContainer<String, Object> repliesContainer) {
-
         return new ReplyingKafkaTemplate<>(pf, repliesContainer);
     }
 
     @Bean
     public ConcurrentMessageListenerContainer<String, Object> repliesContainer(
             ConcurrentKafkaListenerContainerFactory<String, Object> containerFactory) {
-
-        ConcurrentMessageListenerContainer<String, Object> repliesContainer = containerFactory.createContainer(REPLY_TOPICS);
+        ConcurrentMessageListenerContainer<String, Object> repliesContainer = containerFactory.createContainer("REPLY_TOPIC");
         repliesContainer.setAutoStartup(false);
+
         return repliesContainer;
     }
 }
